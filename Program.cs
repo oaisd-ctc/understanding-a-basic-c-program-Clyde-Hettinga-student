3e6d9ba4a4f7e2dc8a1782b00da25b9ab344041b
Clyde-Hettinga-student <nahe406@student.oaisd.org>
2024-10-07 17:24:57 +0000

    Made a "Hello, World!" program

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Clyde Hettinga.");
+
+        //Using Variables
+        int age = 17;
+        string greeting = "Hello World";
+        string firstname = "Clyde";
+        string lastname = "Hettinga";
+        double money = 3.18;
+
+        Console.WriteLine(greeting + "! My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on a large cake pop.");
+    }
+}
